87f61a006b95cc3a1d2d2bb9d312ccec29dad476
Jaime <104177104+JaimeG3@users.noreply.github.com>
2022-05-02 20:11:36 -0500

    modulo 1

---
+++ modulo1/clase2.js
@@ -0,0 +1,32 @@
+//variables
+var nombre = "jaime"
+//imprimir valor de variable
+console.log(nombre)
+//tipo de datos
+//numerico
+var edad = 22
+var precioMoto = 750000
+//texto
+var array = "prueba piloto"
+var array2 = "quiero trabajo"
+//booleanos
+var boolean = true
+var boolean2 = false
+//listas 
+var lista = ["modulo 1", "modulo 2", "modulo 3"]
+var lista2 = [true, true, false]
+var lista3 = [true, 100, "hola"]
+//sacar un elemento de una lista
+var extraccion = lista[2]
+//constantes
+const pi = 3,14
+//modificadores
+var numero4 = 200
+//copia
+var copia = numero4
+numero4 = numero4 - 100
+//texto
+var hola = "hola"
+var como = "como"
+var estas = "estas"
+var saludo = `${hola} ${como} ${estas}`
