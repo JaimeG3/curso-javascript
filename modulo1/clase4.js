c31622cabe54a3366fd97fbf065c694b8e7e11f3
Jaime <104177104+JaimeG3@users.noreply.github.com>
2022-05-10 11:23:12 -0500

    clase 4

---
+++ modulo1/clase4.js
@@ -0,0 +1,11 @@
+//condicionales
+//mama le va dar permiso a hija de cine
+var permisoMama 
+var dineroHija = 0
+if (dineroHija > 0) {   
+    permisoMama = true
+    //codigo que se ejecuta solo si la condicion if se cumple
+} else {    
+    permisoMama = false
+}
+console.log(dineroHija)
