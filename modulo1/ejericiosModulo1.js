ab725e791fc7504cd8f0c3e9cd790ad56f31a712
Jaime <104177104+JaimeG3@users.noreply.github.com>
2023-02-24 15:35:23 -0500

    Update ejericiosModulo1.js

--- modulo1/ejericiosModulo1.js
+++ modulo1/ejericiosModulo1.js
@@ -1,4 +1,4 @@
-//1. Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+/*1. Pide la edad y si es mayor de 18 años indica que ya puede conducir.
 let pedir = prompt("dame la edad")
 if(Number(pedir)==pedir){
 if (pedir >= 18 ){
@@ -7,7 +7,7 @@ if (pedir >= 18 ){
     document.write("no puedes conducir <br />")
 }
 }
-
+*/
 
 /*2. Pide una nota (número). Muestra la calificación según la nota:
 
@@ -16,6 +16,24 @@ if (pedir >= 18 ){
 5-6: Suficiente
 6-7: Bien
 7-9: Notable
-9-10: Sobresaliente  
+9-10: Sobresaliente  */
 
+let nota = prompt("dame la nota")
+if(Number(nota)==nota){
+if(nota>=3){
+    document.write("Muy deficiente")
+}else if(nota > 3 || nota <=5 ){
+    document.write("Muy deficiente")
+}
+else if(nota > 5 || nota <=6){
+    document.write("Muy deficiente")
+}else if(nota > 6 || nota <=7){
+    document.write("Muy deficiente")
+}else if(nota > 7 || nota <=9){
+    document.write("Muy deficiente")
+}else if(nota > 9 || nota <=10){
+    document.write("Muy deficiente")
+}
+
+}
     
