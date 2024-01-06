49433482998a533a2aee9bf4f239f1205ae36110
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-06 14:26:49 -0500

    prc

--- practiceCodeWars/7kiu.js
+++ practiceCodeWars/7kiu.js
@@ -16,3 +16,16 @@ function openOrSenior(data){
 function openOrSenior(data){
     return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
   }
+
+
+//EVEN OR ODD
+  function oddOrEven(array) {
+    //enter code here
+   let result = ""
+   let sum = array.reduce((accrued,current) => accrued + current,0)
+   return sum % 2 === 0 ? "even" : "odd"
+ }
+  // ...
+ function oddOrEven(arr) {
+    return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+  }
