6066ec54fcc0097496ee5427fb089775e2a53d95
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-03-18 21:57:13 -0500

    update 4kiu

--- practiceCodeWars/4kiu.js
+++ practiceCodeWars/4kiu.js
@@ -18,3 +18,29 @@ function sumIntervals(intervals) {
     totalSum += currentInterval[1] - currentInterval[0]; // Sumar longitud del último intervalo sin superposición
     return totalSum;
   }
+
+  function sumIntervals(xs) {
+    let ys = xs.sort(([a,b], [c,d]) => a-c);
+    let m = -Number.MAX_VALUE;
+    let res = 0;
+    for (let [a,b] of ys) {
+      m = Math.max(m, a);
+      res += Math.max(0, b-m);
+      m = Math.max(m, b);
+    }
+    return res;
+  }
+
+  const sumIntervals = (intervals) => {
+    intervals.sort((p, n) => p[1] - n[1]);
+
+    for (let i = 0; i < intervals.length-1; i++) {
+        if (intervals[i][1] > intervals[i+1][0]) {
+            intervals[i][0] = Math.min(intervals[i][0], intervals[i+1][0])
+            intervals[i][1] = intervals[i+1][1];
+            intervals.splice(i+1,1);
+            i = -1;
+        }
+    }
+    return intervals.reduce((a, v) => a + (v[1]-v[0]),0);
+}
