390ffab8563d51770f55227dfbd693baad88db0a
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-03-22 23:07:35 -0500

    4kiu

--- practiceCodeWars/4kiu.js
+++ practiceCodeWars/4kiu.js
@@ -44,3 +44,15 @@ function sumIntervals(intervals) {
     }
     return intervals.reduce((a, v) => a + (v[1]-v[0]),0);
 }
+
+function sumIntervals(xs) {
+    let ys = xs.sort(([a,b], [c,d]) => a-c);
+    let m = -Number.MAX_VALUE;
+    let res = 0;
+    for (let [a,b] of ys) {
+      m = Math.max(m, a);
+      res += Math.max(0, b-m);
+      m = Math.max(m, b);
+    }
+    return res;
+  }
